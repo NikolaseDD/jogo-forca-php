f1abb20b066e8fb99f065452ba4ee0de5deda45c
Nikolas Eduardo <nikolaseduardo619@email.com>
2026-06-20 12:16:04 -0300

    v1.3.0 - Controle de erros e tentativas

--- index.php
+++ index.php
@@ -2,21 +2,40 @@
 session_start();
 
 $palavra = "CASAS";
+$maxErros = 6;
 
 if (!isset($_SESSION['chutes'])) {
     $_SESSION['chutes'] = [];
 }
 
+if (isset($_POST['reiniciar'])) {
+    $_SESSION['chutes'] = [];
+}
+
 if (isset($_POST['letra'])) {
 
-    $letra = strtoupper($_POST['letra']);
+    $letra = strtoupper(trim($_POST['letra']));
 
-    if (!in_array($letra, $_SESSION['chutes'])) {
+    if (
+        !empty($letra) &&
+        !in_array($letra, $_SESSION['chutes'])
+    ) {
         $_SESSION['chutes'][] = $letra;
     }
 }
 
 $letras = str_split($palavra);
+
+$erros = [];
+
+foreach ($_SESSION['chutes'] as $chute) {
+
+    if (!in_array($chute, $letras)) {
+        $erros[] = $chute;
+    }
+}
+
+$tentativasRestantes = $maxErros - count($erros);
 ?>
 
 <!DOCTYPE html>
@@ -28,40 +47,61 @@ $letras = str_split($palavra);
 </head>
 <body>
 
-    <h1>Jogo da Forca</h1>
+<h1>Jogo da Forca</h1>
 
-    <div class="forca">
-        <?php
+<div class="forca">
 
-        foreach ($letras as $letra) {
+<?php
+foreach ($letras as $letra) {
 
-            if (in_array($letra, $_SESSION['chutes'])) {
-                echo $letra . " ";
-            } else {
-                echo "_ ";
-            }
+    if (in_array($letra, $_SESSION['chutes'])) {
+        echo $letra . " ";
+    } else {
+        echo "_ ";
+    }
+}
+?>
 
-        }
+</div>
 
-        ?>
-    </div>
+<br>
 
-    <br>
+<form method="POST">
+    <input type="text" name="letra" maxlength="1" required>
+    <button type="submit">Chutar</button>
+</form>
 
-    <form method="POST">
-        <input type="text" name="letra" maxlength="1" required>
-        <button type="submit">Chutar</button>
-    </form>
+<h3>Letras digitadas:</h3>
 
-    <h3>Letras digitadas:</h3>
+<p>
+<?php
+foreach ($_SESSION['chutes'] as $chute) {
+    echo $chute . " ";
+}
+?>
+</p>
 
-    <p>
-        <?php
-        foreach ($_SESSION['chutes'] as $chute) {
-            echo $chute . " ";
-        }
-        ?>
-    </p>
+<h3>Erros:</h3>
+
+<p>
+<?php
+foreach ($erros as $erro) {
+    echo $erro . " ";
+}
+?>
+</p>
+
+<h3>Tentativas restantes:</h3>
+
+<p>
+<?php echo $tentativasRestantes; ?>
+</p>
+
+<form method="POST">
+    <button type="submit" name="reiniciar">
+        Reiniciar Jogo
+    </button>
+</form>
 
 </body>
 </html>
