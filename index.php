a669988c683cb70d8d9cef30f82ef3adcf19bc82
Nikolas Eduardo <nikolaseduardo619@email.com>
2026-06-20 11:45:22 -0300

    feat: cria estrutura inicial do jogo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Jogo da Forca PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+        <h1>Jogo da Forca</h1>
+
+        <h2>Palavra:</h2>
+
+        <div class="palavra">
+            _ _ _ _
+        </div>
+
+        <form>
+            <input type="text" maxlength="1" placeholder="Digite uma letra">
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+
+</body>
+</html>
